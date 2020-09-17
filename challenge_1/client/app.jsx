81a966423424682d1b37cc07a4c3b7eea4736aff
Shirley Wu <shirleyswwu@gmail.com>
2020-09-16 23:09:52 -0700

    made search field and connect to server

--- challenge_1/client/app.jsx
+++ challenge_1/client/app.jsx
@@ -1,4 +1,3 @@
-// import React, { Component } from 'react';
 import React, { useState, useEffect } from 'react';
 import ReactPaginate from 'react-paginate';
 import db from '../data/db.json';
@@ -10,7 +9,7 @@ const App = () => {
 
    const handleSubmit = () => {
       event.preventDefault();
-      alert('A name was submitted: ' + this.state.value);
+      alert('you submitted: ' + value);
       // make get request to server
       setValue('');
    }
@@ -19,52 +18,18 @@ const App = () => {
       setValue(e.target.value);
    }
 
-   return(
+   return (
       <div>
          <h2>Historical Events Search Engine</h2>
          <form onSubmit={handleSubmit}>
             <label>
                <input type="text" value={value} onChange={handleChange}></input>
-            {/* <button onSubmit={this.handleSubmit}>Search</button> */}
                <input type="submit" value="Search" />
             </label>
          </form>
       </div>
    )
 }
-// class App extends Component{
-//    constructor() {
-//       super()
-//       this.state = {
-//          value: ''
-//       }
-//    this.handleChange = this.handleChange.bind(this);
-//    this.handleSubmit = this.handleSubmit.bind(this);
-//    }
-
-//    handleChange() {
-//       this.setState({value: event.target.value});
-//    }
-
-//    handleSubmit(event) {
-//       event.preventDefault();
-//       alert('A name was submitted: ' + this.state.value);
-//       // make get request to server
-//       this.setState({value: ''});
-//    }
-
-//    render(){
-//       return(
-//          <div>
-//             <h1>Historical Events Search Engine</h1>
-//             <label>
-//                <input type="text" value={this.state.value} onChange={this.handleChange}></input>
-//                <button onSubmit={this.handleSubmit}>Search</button>
-//             </label>
-//          </div>
-//       );
-//    }
-// }
 
 /* Full-text search
 Add q
